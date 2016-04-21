57f9d754c1b4d27c32a67e53dd7dacaa952d5491
Neo Lee <neo.lee@gmail.com>
2016-04-21 18:42:37 +0800

    Update packages to newest version

--- project.clj
+++ project.clj
@@ -4,11 +4,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.codehaus.jsr166-mirror/jsr166y "1.7.0"]
-                 [org.apache.commons/commons-math3 "3.2"]
-                 [org.clojure/clojure "1.7.0"]
-                 [criterium "0.4.3"]
+                 [org.apache.commons/commons-math3 "3.6.1"]
+                 [org.clojure/clojure "1.8.0"]
+                 [criterium "0.4.4"]
                  [org.clojure/math.combinatorics "0.1.1"]
                  [org.clojure/math.numeric-tower "0.0.4"]
-                 [uncomplicate/clojurecl "0.1.2"]
                  ]
   :jvm-opts ["-Xmx1g"])
