8ecf09e1104934ff447009c448f5f88778721830
Neo Lee <neo.lee@gmail.com>
2017-03-27 13:28:03 +0800

    Update all deps

--- project.clj
+++ project.clj
@@ -7,7 +7,8 @@
                  [org.apache.commons/commons-math3 "3.6.1"]
                  [org.clojure/clojure "1.8.0"]
                  [criterium "0.4.4"]
-                 [org.clojure/math.combinatorics "0.1.1"]
+                 [org.clojure/math.combinatorics "0.1.4"]
                  [org.clojure/math.numeric-tower "0.0.4"]
                  ]
+  :plugins [[lein-clean-m2 "0.1.2"]]
   :jvm-opts ["-Xmx1g"])
