8dbd2d74f7aeef2022e0aacc13ced78e7a22fbd7
Neo Lee <neo.lee@gmail.com>
2017-03-29 00:48:47 +0800

    Remove white spaces

--- 2011.clj
+++ 2011.clj
@@ -1,16 +1,16 @@
 (defn div? [n d]
   (zero? (rem n d)))
- 
+
 (defn smallest-prime-factor [number]
   (loop [n number d 2]
     (cond (> d (int (Math/sqrt number))) n
           (= n d) n
           (div? n d) d
           true (recur n (inc d)))))
- 
+
 (defn prime? [n]
   (= n (smallest-prime-factor n)))
- 
+
 (def primes (lazy-cat '(2 3)
 		      (filter prime? (take-nth 2 (iterate inc 5)))))
 
@@ -22,4 +22,3 @@
 (def magic_primes (for [n eleven_primes_sums :when (prime? n)] n))
 
 (println (nth magic_primes 1000))
-
