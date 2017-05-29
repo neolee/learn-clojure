8f64b7bc47a471532e0cc8bf55e8b14ab53e7051
Neo Lee <neo.lee@gmail.com>
2017-05-29 20:52:24 +0800

    Update deps

--- project.clj
+++ project.clj
@@ -3,9 +3,8 @@
   :url "http://paradigmx.net/clojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.codehaus.jsr166-mirror/jsr166y "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.apache.commons/commons-math3 "3.6.1"]
-                 [org.clojure/clojure "1.8.0"]
                  [criterium "0.4.4"]
                  [org.clojure/math.combinatorics "0.1.4"]
                  [org.clojure/math.numeric-tower "0.0.4"]
