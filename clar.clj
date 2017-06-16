88bbbdb1ff3992bea65563a118cc7f803bcc1aee
Neo Lee <neo.lee@gmail.com>
2017-06-16 15:56:39 -0400

    Learn some LA through neanderthal lib

---
+++ clar.clj
@@ -0,0 +1,76 @@
+; Blog: http://dragan.rocks/articles/17/Clojure-Linear-Algebra-Refresher-Vector-Spaces
+(require '[uncomplicate.neanderthal
+           [native :refer :all]
+           [core :refer :all]
+           [linalg :refer :all]])
+
+; Vectors, addition and scalar multiplication
+(def v1 (dv -1 2 5.2 0))
+(def v2 (dv (range 22)))
+(def v3 (dv -2 -3 1 0))
+(xpy v1 v3)
+(scal 2.5 v1)
+(axpy! 2.5 v1 v3)
+
+(dv 7)
+(zero v2)
+(def v4 (scal -1 v1))
+(axpby! 1 v1 -1 v3)
+
+; Linear combinations
+(let [u (dv 2 5 -3)
+      v (dv -4 1 9)
+      w (dv 4 0 2)]
+  (axpy 2 u -3 v 1 w))
+
+; Dot product
+(let [u (dv 1 -2 4)
+      v (dv 3 0 2)]
+  (dot u v))
+; Norm
+(nrm2 (dv 1 3 5))
+; Construction of unit vector
+(let [v (dv 1 3 5)]
+  (scal (/ (nrm2 v)) v))
+; Angle
+(let [u (dv 1 0 0)
+      v (dv 1 0 1)]
+  (/ (dot u v) (nrm2 u) (nrm2 v)))
+; Because cos(pi/4)=0...(i.e orthogonal)
+(dot (dv 2 -3 1) (dv 1 2 4))
+; Distance
+(let [x (dv 4 0 -3 5)
+      y (dv 1 -2 3 0)]
+  (nrm2 (axpy -1 y x)))
+
+; Matrix
+(def m (dge 2 3 (range 6)))
+(nrm2 m)
+
+; Linear dependence
+(let [x (dv 2 -1 3)
+      y (dv 4 -2 6)]
+  (/ (dot x y) (nrm2 x) (nrm2 y)))
+
+; Solving systems of linear equations
+(let [cs (dge 3 1)]
+  (sv! (dge 3 3 [1 2 0
+                 0 1 -1
+                 1 1 2])
+       cs)
+  cs)
+
+; Rank
+(let [a (dge 4 4 [1 0 0 0
+                  2 0 0 0
+                  0 1 0 0
+                  0 0 1 0])
+      s (dv 4)
+      superb (dv 4)]
+  (svd! a s superb)
+  s)
+
+; Orthonormal vectors and projections
+(let [v (dv 6 7)
+      u (dv 1 4)]
+  (scal (/ (dot u v) (dot u u)) u))
--- project.clj
+++ project.clj
@@ -4,8 +4,11 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/tools.logging "0.4.0"]
+                 [ch.qos.logback/logback-classic "1.2.3"]
                  [org.apache.commons/commons-math3 "3.6.1"]
                  [criterium "0.4.4"]
+                 [uncomplicate/neanderthal "0.13.1"]
                  [org.clojure/math.combinatorics "0.1.4"]
                  [org.clojure/math.numeric-tower "0.0.4"]
                  ]
