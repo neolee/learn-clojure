cf7fbb4f4d59acd33faa515ed2d35c38cd7181d0
Neo Lee <neo.lee@gmail.com>
2018-12-17 11:59:56 +0800

    Update to Clojure 1.9.0

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://paradigmx.net/clojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/tools.logging "0.4.1"]
                  [ch.qos.logback/logback-classic "1.2.3"]
                  [org.apache.commons/commons-math3 "3.6.1"]
