459cc74730f9b427d34c7d17ba24e9b22092058e
Neo Lee <neo.lee@gmail.com>
2019-08-10 13:25:12 +0800

    Update assets

--- project.clj
+++ project.clj
@@ -4,12 +4,12 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/tools.logging "0.5.0-alpha.1"]
+                 [org.clojure/tools.logging "0.5.0"]
                  [ch.qos.logback/logback-classic "1.2.3"]
                  [org.apache.commons/commons-math3 "3.6.1"]
-                 [uncomplicate/neanderthal "0.25.3" :exclusions [org.jcuda/jcuda-natives
+                 [uncomplicate/neanderthal "0.25.6" :exclusions [org.jcuda/jcuda-natives
                                                                  org.jcuda/jcublas-natives]]
-                 [org.clojure/math.combinatorics "0.1.5"]
+                 [org.clojure/math.combinatorics "0.1.6"]
                  [org.clojure/math.numeric-tower "0.0.4"]
                  ]
   :resource-paths ["resources"])
