d8cd5edaa090ebf87fe9871f8947984560c0ba44
Neo Lee <neo.lee@gmail.com>
2020-12-10 12:05:10 +0800

    Update assets

--- project.clj
+++ project.clj
@@ -5,9 +5,9 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/tools.logging "1.1.0"]
-                 [ch.qos.logback/logback-classic "1.3.0-alpha4"]
+                 [ch.qos.logback/logback-classic "1.3.0-alpha5"]
                  [org.apache.commons/commons-math3 "3.6.1"]
-                 [uncomplicate/neanderthal "0.32.0" :exclusions [org.jcuda/jcuda-natives
+                 [uncomplicate/neanderthal "0.38.0" :exclusions [org.jcuda/jcuda-natives
                                                                  org.jcuda/jcublas-natives]]
                  [org.clojure/math.combinatorics "0.1.6"]
                  [org.clojure/math.numeric-tower "0.0.4"]
